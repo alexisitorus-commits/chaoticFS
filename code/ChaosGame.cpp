9c1e47b8bb56064cfc699d8ef244bc8eda680d02
alexsit <alexisitorus@gmail.com>
2025-11-22 12:56:41 -0800

    code

---
+++ code/ChaosGame.cpp
@@ -0,0 +1,148 @@
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+//Make the code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+	// Create and open a window for the game
+	RenderWindow window(vm, "Chaos Game!!", Style::Default);
+
+	vector<Vector2f> vertices;
+	vector<Vector2f> points;
+	//font appear NOW
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+	{
+		cout << "font isn't real sorry.";
+	}
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString("Please click 3 vertices, then click a starting point!");
+	text.setCharacterSize(24);
+	text.setFillColor(sf::Color::Red);
+	text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+	text.setPosition(20, 20);
+
+
+	while (window.isOpen())
+	{
+		/*
+		****************************************
+		Handle the players input
+		****************************************
+		*/
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				// Quit the game when the window is closed
+				window.close();
+			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					std::cout << "the left button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+
+					if (vertices.size() < 3)
+					{
+						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+					//4th clickth
+					else if (points.size() == 0)
+					{
+						///fourth click
+						///push back to points vector
+						points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+				}
+			}
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		/*
+		****************************************
+		Updated
+		****************************************
+		*/
+
+		if (points.size() > 0)
+		{
+			///generate more point(s)
+			///select random vertex
+			///calculate midpoint between random vertex and the last point
+			/// in the vector
+			///push back the newly generated coord.
+			for (int i = 0; i < 50; i++)
+			{
+				//select random vertex
+				int r = rand() % 3;
+
+				//the last point of the vector
+				Vector2f last = points.back();
+				Vector2f target = vertices[r];
+
+				//midpointin 
+				Vector2f mid((last.x + target.x) / 2.0f, (last.y + target.y) / 2.0f);
+
+				points.push_back(mid);
+			}
+		}
+		//follows users advances and like program talks to user
+		if (vertices.size() < 3)
+		{
+			text.setString("Click 3 vertices for the triangle");
+		}
+		else if (points.size() == 0)
+		{
+			text.setString("Click a starting point");
+		}
+		else
+		{
+			text.setString("Generating Sierpinski Triangle...");
+		}
+
+
+		/*
+		****************************************
+		Draw
+		****************************************
+		*/
+		window.clear();
+		for (int i = 0; i < vertices.size(); i++)
+		{
+			RectangleShape rect(Vector2f(10, 10));
+			rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+			rect.setFillColor(Color::Blue);
+			window.draw(rect);
+		}
+
+		for (int i = 0; i < points.size(); i++)
+		{
+			RectangleShape pixel(Vector2f(2, 2));
+			pixel.setPosition(points[i]);
+			pixel.setFillColor(Color::White);
+			window.draw(pixel);
+		}
+
+		//text drawing
+		window.draw(text);
+		///TODO:  Draw points
+		window.display();
+	}
+}
